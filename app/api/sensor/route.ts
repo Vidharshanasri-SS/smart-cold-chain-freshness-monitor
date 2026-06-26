362311f11ce319e05921320708c42fbf864d2b94
Vidharshana Sri S S <vidharshanasri.ss@gmail.com>
2026-06-26 17:29:36 +0530

    Update route.ts

--- app/api/sensor/route.ts
+++ app/api/sensor/route.ts
@@ -11,7 +11,7 @@ let sensorData = {
 export async function POST(request: Request) {
   try {
     const body = await request.json();
-
+ 
     sensorData = {
       temperature: body.temperature,
       humidity: body.humidity,
